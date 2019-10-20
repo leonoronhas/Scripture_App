f431569a4825e5a3d4a58f93393b2e8910d32c53
leonoronsantos <leosud099@gmail.com>
2019-10-19 21:39:35 -0600

    Final, but LOAD still broke

--- app/src/main/java/leonoronhas/scriptureapp/DisplayScripture.java
+++ app/src/main/java/leonoronhas/scriptureapp/DisplayScripture.java
@@ -2,11 +2,21 @@ package leonoronhas.scriptureapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DisplayScripture extends AppCompatActivity {
+
+    String book_input;
+    String chapter_input;
+    String verse_input;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +26,28 @@ public class DisplayScripture extends AppCompatActivity {
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
 
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        String message2 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE2);
-        String message3 = intent.getStringExtra(MainActivity.EXTRA_MESSAGE3);
+        book_input = intent.getStringExtra(MainActivity.EXTRA_book_input);
+        chapter_input = intent.getStringExtra(MainActivity.EXTRA_chapter_input);
+        verse_input = intent.getStringExtra(MainActivity.EXTRA_verse_input);
+
+        String scripture = book_input + " " + chapter_input + ":" + verse_input;
 
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
 
-        textView.setText(message + " " + message2 + ":" + message3);
+        textView.setText(scripture);
     }
 
+    public void save(View view) {
+        SharedPreferences sharedPreferences = this.getSharedPreferences(MainActivity.FILE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(MainActivity.EXTRA_book_input, book_input);
+        editor.putString(MainActivity.EXTRA_chapter_input, chapter_input);
+        editor.putString(MainActivity.EXTRA_verse_input, verse_input);
+        editor.apply();
+
+        Toast.makeText(this, "Scripture saved successfully!", Toast.LENGTH_SHORT).show();
+    }
 
 }
--- app/src/main/java/leonoronhas/scriptureapp/MainActivity.java
+++ app/src/main/java/leonoronhas/scriptureapp/MainActivity.java
@@ -2,17 +2,24 @@ package leonoronhas.scriptureapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Why do I need this line?
-    public final static String EXTRA_MESSAGE = "leonoronhas.scriptureapp.MESSAGE";
-    public final static String EXTRA_MESSAGE2 = "leonoronhas.scriptureapp.MESSAGE2";
-    public final static String EXTRA_MESSAGE3 = "leonoronhas.scriptureapp.MESSAGE3";
+    public final static String EXTRA_book_input = "leonoronhas.scriptureapp.book_input";
+    public final static String EXTRA_chapter_input = "leonoronhas.scriptureapp.chapter_input";
+    public final static String EXTRA_verse_input = "leonoronhas.scriptureapp.verse_input";
+    public final static String FILE_NAME = "DEFAULT NAME";
+
+    EditText bookId;
+    EditText chapterId;
+    EditText verseId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +27,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendScripture(View view){
+    public void sendScripture(View view) {
         Intent intent = new Intent(this, DisplayScripture.class);
-        EditText editText = findViewById(R.id.editBook);
-        EditText editText2 = findViewById(R.id.editChapter);
-        EditText editText3 = findViewById(R.id.editVerse);
+        bookId = findViewById(R.id.editBook);
+        chapterId = findViewById(R.id.editChapter);
+        verseId = findViewById(R.id.editVerse);
 
-        String message1 = editText.getText().toString();
-        String message2 = editText2.getText().toString();
-        String message3 = editText3.getText().toString();
+        String book_input1 = bookId.getText().toString();
+        String chapter_input = chapterId.getText().toString();
+        String verse_input = verseId.getText().toString();
 
 
-        intent.putExtra(EXTRA_MESSAGE, message1);
-        intent.putExtra(EXTRA_MESSAGE2, message2);
-        intent.putExtra(EXTRA_MESSAGE3, message3);
+        intent.putExtra(EXTRA_book_input, book_input1);
+        intent.putExtra(EXTRA_chapter_input, chapter_input);
+        intent.putExtra(EXTRA_verse_input, verse_input);
 
         startActivity(intent);
+    }
+
+    public void loadScripture(View view) {
+        SharedPreferences sharedPreferences = this.getSharedPreferences(MainActivity.FILE_NAME, Context.MODE_PRIVATE);
+
+        String book = sharedPreferences.getString(EXTRA_book_input, "");
+        String chapter = sharedPreferences.getString(EXTRA_chapter_input, "");
+        String verse = sharedPreferences.getString(EXTRA_verse_input, "");
+
+        bookId.setText(book);
+        chapterId.setText(chapter);
+        verseId.setText(verse);
+
     }
 }
